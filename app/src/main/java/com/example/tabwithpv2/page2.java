db52c5bdaa6c434ee9ba1f111cad2a9577e5cfaa
impooh121 <impooh121@kaist.ac.kr>
2021-12-30 04:37:28 +0900

    b1

--- app/src/main/java/com/example/tabwithpv2/page2.java
+++ app/src/main/java/com/example/tabwithpv2/page2.java
@@ -1,11 +1,27 @@
 package com.example.tabwithpv2;
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.tabwithpv2.databinding.FragmentPage2Binding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +38,9 @@ public class page2 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    final int GET_GALLERY_IMAGE = 200;
+    page2Adapter adapter;
+    FragmentPage2Binding binding;
 
     public page2() {
         // Required empty public constructor
@@ -54,10 +73,59 @@ public class page2 extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page2, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_page2, container, false);
+        ArrayList<pic> list = new ArrayList<>();
+
+        for (int i=0; i<3; i++) {
+            list.add(new pic(R.drawable.ic_launcher_foreground,null)) ;
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = rootview.findViewById(R.id.page2RecyclerViewId) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL, false));
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        adapter = new page2Adapter(list) ;
+        recyclerView.setAdapter(adapter) ;
+
+        binding = FragmentPage2Binding.inflate(getLayoutInflater());
+        //FloatingActionButton fab = binding.page2FloatingActionButton;
+        FloatingActionButton fab = rootview.findViewById(R.id.page2FloatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                //adapter.addItem(new pic(R.drawable.ic_launcher_background,null));
+                adapter.notifyDataSetChanged();
+                Toast.makeText(getContext(), "hi!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+
+
+        return rootview;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            adapter.addItem(new pic(0,selectedImageUri));
+            adapter.notifyDataSetChanged();
+
+        }
+        else{
+            Toast.makeText(getContext(), "load failed!", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
