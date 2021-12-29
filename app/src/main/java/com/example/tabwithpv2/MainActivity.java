2e7411ea91fba08bc5b2e514815a8ff865db9518
impooh121 <impooh121@kaist.ac.kr>
2021-12-29 17:33:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabwithpv2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tabwithpv2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //< create >
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //</ create >
+
+        //< get elements >
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
+        //</ get elements >
+
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Tab " + (position + 1));
+                    }
+                }).attach();
+    }
+}
---
+++ app/src/main/java/com/example/tabwithpv2/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.tabwithpv2;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity)
+    {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position) {
+            case 0:
+                return  new page1();
+            case 1:
+                return  new page2();
+            default:
+                return  new page3();
+        }
+    }
+    @Override
+    public int getItemCount() {return 3; }
+}
