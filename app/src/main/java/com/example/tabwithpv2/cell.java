b3f1fdc48bfaf3ef45bf2661c6071d9ebdf16b01
DDoubleA <ace9804@unist.ac.kr>
2021-12-31 15:45:22 +0900

    j11

---
+++ app/src/main/java/com/example/tabwithpv2/cell.java
@@ -0,0 +1,11 @@
+package com.example.tabwithpv2;
+
+public class cell {
+    int x,y;
+    float l;
+    cell(int x,int y,float l){
+        this.x=x;
+        this.y=y;
+        this.l=l;
+    }
+}
--- app/src/main/java/com/example/tabwithpv2/page3.java
+++ app/src/main/java/com/example/tabwithpv2/page3.java
@@ -3,10 +3,18 @@ package com.example.tabwithpv2;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.tabwithpv2.databinding.FragmentPage2Binding;
+import com.example.tabwithpv2.databinding.FragmentPage3Binding;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +31,9 @@ public class page3 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    page3Adapter adapter;
+    FragmentPage3Binding binding;
+    int sz;
     public page3() {
 
     }
@@ -59,6 +69,27 @@ public class page3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page3, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_page3, container, false);
+        RecyclerView recyclerView = rootview.findViewById(R.id.page3RecyclerViewId) ;
+        float l= 80.0F;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            l = Float.min((float) recyclerView.getWidth(), (float) recyclerView.getHeight());
+        }
+        Toast.makeText(getContext(), "!!"+recyclerView.getWidth(), Toast.LENGTH_SHORT).show();
+        ArrayList<cell> list = new ArrayList<>();
+        sz=5;
+        for (int i=0; i<sz*sz; i++) {
+            list.add(new cell(i/sz,i%sz,l/(float)sz)) ;
+        }
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),sz,GridLayoutManager.VERTICAL,false));
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        adapter = new page3Adapter(list) ;
+        recyclerView.setAdapter(adapter) ;
+
+        binding = FragmentPage3Binding.inflate(getLayoutInflater());
+        return rootview;
     }
 }
