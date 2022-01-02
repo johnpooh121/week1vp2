26ea7e0e999996f5631d342f8c07efee6fc544da
impooh121 <impooh121@kaist.ac.kr>
2022-01-02 18:48:33 +0900

    fragment방식

--- app/src/main/java/com/example/tabwithpv2/gamegrid.java
+++ app/src/main/java/com/example/tabwithpv2/gamegrid.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link gamegrid#newInstance} factory method to
@@ -73,6 +75,16 @@ public class gamegrid extends Fragment {
         }
     }
 
+    public String conv(int x){
+        x=x%256;
+        return Integer.toHexString(x/16)+""+Integer.toHexString(x%16);
+    }
+
+    public int change(int x,int d){
+        if(x+d>256)return x-d;
+        return x+d;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -92,12 +104,23 @@ public class gamegrid extends Fragment {
         params.setMargins(5,5,5,5);
         params.weight=1;
         //LayoutParams layoutParams = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
+        Random rd = new Random();
+        int R = rd.nextInt(256);
+        int G = rd.nextInt(256);
+        int B = rd.nextInt(256);
+        int d =25+rd.nextInt(10);
+        int nR=change(R,d),nG=change(G,d),nB=change(B,d);
+
+        String bg = "#"+conv(R)+conv(G)+conv(B);
+        String col = bg;
+        col = "#"+conv(nR)+conv(nG)+conv(nB);
+        //String col = "#"+Integer.toHexString((R+d)%256)+Integer.toHexString((G+d)%256)+Integer.toHexString((B+d)%256);
         for(int i=1;i<=l;i++){
             for(int j=1;j<=l;j++){
                 Button v = new Button(getContext());
-                v.setBackgroundColor(Color.parseColor("#00ff00"));
+                v.setBackgroundColor(Color.parseColor(bg));
                 if(i==x&&j==y){
-                    v.setBackgroundColor(Color.parseColor("#000000"));
+                    v.setBackgroundColor(Color.parseColor(col));
                 }
                 v.setLayoutParams(params);
                 int finali = i;
